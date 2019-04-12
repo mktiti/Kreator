533eb78ccad885d9086b4671610732960753bf92
Márkus Krisztián <markuskrisztian1@gmail.com>
2019-04-13 00:45:51 +0200

    Typo fix

--- kreator-annotation/src/main/java/exception.kt
+++ kreator-annotation/src/main/java/exception.kt
@@ -2,6 +2,6 @@ package hu.mktiti.kreator.annotation
 
 class InjectorException : RuntimeException("Cannot find injection provider, kreator-core package may be missing")
 
-class InjectionException(componentName: String) : RuntimeException("Cannot injectable object for $componentName")
+class InjectionException(componentName: String) : RuntimeException("Cannot inject object for $componentName")
 
 class InjectionConfigException(componentName: String) : RuntimeException("Invalid injection provider $componentName")
